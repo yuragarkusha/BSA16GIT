4a6fec9447ff64424467d5f96c4fdf7a6e80552d
Ge0rge <yura.garik@gmail.com>
2016-05-24 23:39:36 +0300

    Add Calculator class

---
+++ BSA16GIT/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSA16GIT
+{
+    public static class Calculator
+    {
+        public static int Plus(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            int c = a - b;
+            return c;
+        }
+    }
+}
--- BSA16GIT/Program.cs
+++ BSA16GIT/Program.cs
@@ -10,6 +10,15 @@ namespace BSA16GIT
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите  1 число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите  2 число:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int plusValue = Calculator.Plus(a,b);
+            int minusValue = Calculator.Minus(a, b);
+            Console.WriteLine("Сумма чисел: "+ plusValue);
+            Console.WriteLine("Разница чисел: " + minusValue);
+            Console.ReadLine();
         }
     }
 }
